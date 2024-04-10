cc7a60b70dfff47bc81ec096460ddb549b4c81fc
selosele <osh3384@naver.com>
2024-04-10 18:04:19 +0900

    탑네비게이션 구성

--- src/components/Layout/TopNav/TopNav.tsx
+++ src/components/Layout/TopNav/TopNav.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
@@ -5,12 +7,55 @@ import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
-import { useNavigate } from 'react-router-dom'
+import { CssBaseline, Divider, Drawer, List, ListItem, ListItemButton, ListItemText } from '@mui/material'
 import { messageUtil } from '@/utils'
 
+interface Props {
+  window?: () => Window
+}
+
+const drawerWidth = 240
+const siteTitle = '블로그 ADMIN 시스템'
+const navItems = [
+  '공통코드 관리',
+  '메뉴 관리',
+  '카테고리/태그 관리',
+  '포스트 댓글 관리',
+  '콘텐츠 관리',
+  '만족도조사 관리',
+  '검색 관리',
+  '프로그램 관리'
+]
+
 /** 레이아웃 탑메뉴 컴포넌트 */
-export default function TopMenu() {
+export default function TopMenu(props: Props) {
   const navigate = useNavigate()
+  const [mobileOpen, setMobileOpen] = useState(false)
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState)
+  }
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        <Link to="/">{siteTitle}</Link>
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  )
+
+  const { window } = props
+  const container = window !== undefined ? () => window().document.body : undefined
 
   /** 로그아웃 */
   const logOut = async (e: React.MouseEvent<HTMLElement>) => {
@@ -19,29 +64,58 @@ export default function TopMenu() {
     const confirm = await messageUtil.confirmQuestion('로그아웃하시겠습니까?')
     if (!confirm) return
 
-    window.localStorage.removeItem('accessToken')
+    localStorage.removeItem('accessToken')
     navigate('/login')
   }
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
+        <IconButton
             color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            블로그 ADMIN 시스템
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            <Link to="/">{siteTitle}</Link>
           </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
           <Button color="inherit" onClick={logOut}>Logout</Button>
         </Toolbar>
       </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
     </Box>
   )
 }
