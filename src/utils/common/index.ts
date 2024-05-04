ddc49c6ae9cec8e010e10d79c6ef9e3d23953737
selosele <osh3384@naver.com>
2024-05-04 21:22:46 +0900

    리팩토링 및 유틸함수 추가

--- src/utils/common/index.ts
+++ src/utils/common/index.ts
@@ -1,9 +1,11 @@
+import { useNavigate } from "react-router-dom"
+
 /** 블로그 URL */
 export const BLOG_URL = 'https://blog.selosele.com'
 
 /** 값이 비었는지 확인한다. */
 export function isEmpty(value: any): boolean {
-  return null === value || undefined === value || '' === value
+  return value === null || value === undefined || value === ''
 }
 
 /** 값이 있는지 확인한다. */
@@ -13,10 +15,21 @@ export function isNotEmpty(value: any): boolean {
 
 /** 문자열 값이 비었는지 확인한다. */
 export function isBlank(value: any): boolean {
-  return isEmpty(value) || 0 === value.trim().length
+  return isEmpty(value) || value.trim().length === 0
 }
 
 /** 문자열 값이 있는지 확인한다. */
 export function isNotBlank(value: any): boolean {
   return !isBlank(value)
+}
+
+/** 깊은 복사를 해서 반환한다. */
+export function deepCopy(value: any): any {
+  return JSON.parse(JSON.stringify(value))
+}
+
+/** 페이지를 이동한다. */
+export function handleNavigation(path: string) {
+  const navigate = useNavigate()
+  navigate(path)
 }
