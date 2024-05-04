fc51273360cd9126dee1ed691b161d296c994b41
selosele <osh3384@naver.com>
2024-05-04 21:20:35 +0900

    React.StrictMode 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // React.StrictMode 사용 시, 컴포넌트를 2번 렌더링함
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
