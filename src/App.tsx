e02b7405a1a3fc60cd6a60da9c7e1e9ab5ed3a55
selosele <osh3384@naver.com>
2024-05-04 21:24:12 +0900

    페이지 로드 시, 공통코드 데이터 목록 조회

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { useEffect } from 'react'
+import { ThemeProvider } from '@mui/material'
 import { theme } from './utils'
 import { GlobalRouter } from './routes'
-import { ThemeProvider } from '@mui/material'
+import useCodeStore from './store/code'
 
 import './App.css'
 
 export default function App() {
+  const codeStore = useCodeStore()
+  useEffect(() => {
+    async function fetchData() {
+      await codeStore.listData()
+    }
+    fetchData()
+  }, [])
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalRouter />
