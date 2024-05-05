604771322aecd169d72a7921c7c1bada57bc6062
selosele <osh3384@naver.com>
2024-05-05 13:19:39 +0900

    리팩토링

--- src/components/UI/DataGrid/DataGrid.tsx
+++ src/components/UI/DataGrid/DataGrid.tsx
@@ -1,17 +1,27 @@
-import { DataGrid as DataGridComponent, DataGridProps } from '@mui/x-data-grid'
+import { DataGrid as DataGridComponent, DataGridProps, GridColDef } from '@mui/x-data-grid'
 
 /** 데이터그리드 컴포넌트 */
-export default function DataGrid(props: DataGridProps) {
-  props.columns.forEach(c => {
-    if (c.field === 'rownum') {
-      c.headerName = 'No'
+export default function DataGrid({ columns, rows, ...otherProps }: DataGridProps) {
+  
+  // 컬럼 수정
+  const modifiedColumns: GridColDef[] = columns.map(column => {
+    if (column.field === 'rownum') {
+      return { ...column, headerName: 'No' }
     }
-    props.rows.forEach((r,j) => r.rownum = j+1)
+    return column
   })
+
+  // rownum 추가
+  const modifiedRows = rows.map((row, index) => ({
+    ...row,
+    rownum: index + 1
+  }))
 
   return (
     <DataGridComponent
-      {...props}
+      columns={modifiedColumns}
+      rows={modifiedRows}
+      {...otherProps}
     />
   )
 }
