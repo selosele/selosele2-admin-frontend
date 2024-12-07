48d2ecc5494a2a8dcf9fada2ef948f3e521cc41a
selosele <osh3384@naver.com>
2024-12-07 11:30:04 +0900

    프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/system/',
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
