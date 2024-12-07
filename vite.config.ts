7655afa4eb16f0e17c29cdd8ee495317b8ab46a6
selosele <osh3384@naver.com>
2024-12-07 11:31:37 +0900

    프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,12 @@ export default defineConfig({
     proxy: {
       '/api': 'http://localhost:3000'
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
